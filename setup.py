771f0ef8da5861083f41c727986044a7e9b03255
Vitor dos Santos Barcelos <progvitor@gmail.com>
2024-03-05 14:26:42 -0300

    package packing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='backend_controle_financeiro',
     version='1.0.0',
-    packages=find_packages(exclude='DB'),
+    packages=find_packages(),
     install_requires=[
         'pandas_datareader',
         'pandas',
